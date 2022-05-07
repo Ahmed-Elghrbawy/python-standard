e165a889d9b1f8c6967f50aa9dc74561943ffa59
Ahmed-Elghrbawy <ahmed@codecov.io>
2022-05-07 16:43:09 +0200

    test

--- src/index.py
+++ src/index.py
@@ -4,6 +4,13 @@ def uncovered_if(var=True):
     else:
       return True
 
+
+def some_function(x):
+  if x < 3:
+    return False
+  
+  return True
+
 def fully_covered():
     return True;
 
--- src/test_index.py
+++ src/test_index.py
@@ -8,5 +8,9 @@ def test_fully_covered():
     assert index.fully_covered() == True
 
 
+def test_some_func():
+    assert index.some_function(3)
 
+def test_some_func_less():
+    assert not index.some_function(2)
 
